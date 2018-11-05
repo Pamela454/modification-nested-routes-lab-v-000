545985cfb8860fdb411eaa11b5ff1d5b390e1201
Pamela Frieze <pfrieze@gmail.com>
2018-11-05 21:15:16 +0000

    automatically backed up by learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,5 +1,5 @@
 module SongsHelper
-  def display_drop_down_list(post)
+  def display_drop_down_list(artists)
     if post.author.nil?
       select_tag "post[author_id]", options_from_collection_for_select(Author.all, :id, :name)
     else
